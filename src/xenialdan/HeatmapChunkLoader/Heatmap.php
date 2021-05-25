fe20db432d57943fde98f5b00e97c75a28ef7a32
XenialDan <thebigsmilexd@gmail.com>
2021-05-25 20:15:44 +0200

    Improved threshold code

--- src/xenialdan/HeatmapChunkLoader/Heatmap.php
+++ src/xenialdan/HeatmapChunkLoader/Heatmap.php
@@ -10,6 +10,9 @@ class Heatmap
 {
     /** @var HeatmapEntry[] */
     private array $heatmap = [];
+    public int $total = 0;
+    public int $schedules = 0;
+    private int $maxLoad = 0;
 
     /*public function __construct()
     {
@@ -20,35 +23,65 @@ class Heatmap
     {
         //$this->heatmap->clear();
         $this->heatmap = [];
+        $this->total = 0;
+        $this->schedules = 0;
+        $this->maxLoad = 0;
     }
+
+    public function schedule(): void
+    {
+        $this->schedules++;
+        $this->filter();
+        #$this->total = 0;
+    }
+
+//    public function filter(): void
+//    {
+//        $period = Loader::getInstance()->getPeriod();
+//        foreach ($this->heatmap as $hash => $value) {
+//            // remove if not modified within the period
+//            $old = microtime(true) - $value->lastModified > $period;
+//            $unload = $this->checkUnload($hash);
+//            if ($old || $unload) {
+//                World::getXZ($hash, $x, $z);
+//                Loader::getInstance()->getLogger()->debug("Unregistering loader at $x:$z, Reason:" . ($old ? " unchanged for $period" : '') . ($unload ? ' below treshold' : ''));
+//                $value->getWorld()->unregisterChunkLoader($value, $x, $z);
+//            }
+//        }
+//    }
 
     public function filter(): void
     {
-        $period = Loader::getInstance()->getPeriod();
-        foreach ($this->heatmap as $hash => $value) {
-            // remove if not modified within the period
-            $old = microtime(true) - $value->lastModified > $period;
-            $unload = $this->checkUnload($hash);
-            if ($old || $unload) {
-                World::getXZ($hash, $x, $z);
-                Loader::getInstance()->getLogger()->debug("Unregistering loader at $x:$z, Reason:" . ($old ? " unchanged for $period" : '') . ($unload ? ' below treshold' : ''));
-                $value->getWorld()->unregisterChunkLoader($value, $x, $z);
-                unset($this->heatmap[$hash]);
+        foreach ($this->heatmap as $hash => $entry) {
+            World::getXZ($hash, $x, $z);
+            if ($this->isInTreshold($entry)) {
+                $entry->getWorld()->registerChunkLoader($entry, $x, $z);
+                Loader::getInstance()->getLogger()->debug("Registering loader at $x:$z on load");
+            } else {
+                Loader::getInstance()->getLogger()->debug("Unregistering loader at $x:$z, Reason: below treshold");
+                $entry->getWorld()->unregisterChunkLoader($entry, $x, $z);
             }
         }
     }
 
     public function addLoad(World $world, int $x, int $z): void
     {
+        $this->total++;
         $hash = World::chunkHash($x, $z);
         $entry = $this->heatmap[$hash] ?? new HeatmapEntry($world);
         $entry->lastModified = microtime(true);
         $entry->countLoad++;
+        $this->maxLoad = max($entry->countLoad, $this->maxLoad);
         $this->heatmap[$hash] = $entry;
-        if ($entry->countLoad >= Loader::getInstance()->getTreshold()) {
-            $world->registerChunkLoader($entry, $x, $z);
-            Loader::getInstance()->getLogger()->debug("Registering loader at $x:$z");
-        }
+    }
+
+    private function isInTreshold(HeatmapEntry $entry): bool
+    {
+        //math is: load / max(loads) >= (100% - treshold)
+        $treshold = (100 - Loader::getInstance()->getTreshold()) / 100;
+        $entryTreshold = $entry->countLoad / $this->maxLoad;
+        var_dump($treshold, $entryTreshold);
+        return $entryTreshold >= $treshold;
     }
 
     public function checkUnload($hash): bool
--- src/xenialdan/HeatmapChunkLoader/Loader.php
+++ src/xenialdan/HeatmapChunkLoader/Loader.php
@@ -7,7 +7,6 @@ namespace xenialdan\HeatmapChunkLoader;
 use InvalidArgumentException;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
-use pocketmine\world\ChunkLoader;
 use xenialdan\HeatmapChunkLoader\commands\heatmap\HeatmapCommand;
 
 class Loader extends PluginBase
@@ -31,23 +30,24 @@ class Loader extends PluginBase
         self::$instance = $this;
         $this->heatmap = new Heatmap();
         $this->reloadConfig();
-        $period =  (int)($this->getPeriod()/20);
-        $this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(function() : void{
-            Loader::getInstance()->getHeatmap()->filter();
-        }),$period,$period);
+        $period = $this->getPeriod() * 20;
+        $this->getLogger()->debug("Period = $period");
+        $this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(function (): void {
+            Loader::getInstance()->getHeatmap()->schedule();
+        }), $period, $period);
     }
 
     public function getPeriod(): int
     {
-        $period = strtotime($this->getConfig()->get('period', '5 minutes'));
+        $period = strtotime($this->getConfig()->get('period', '1 minute'), 0);
         if ($period === false) throw new InvalidArgumentException('Config period could not be parsed by strtotime. Check https://www.php.net/manual/en/function.strtotime.php');
-        return $period;
+        return (int)$period;
     }
 
     //TODO add percentage treshold & max chunks setting
     public function getTreshold(): int
     {
-        return $this->getConfig()->get('treshold', 3);
+        return $this->getConfig()->get('threshold', 20);
     }
 
     public function onEnable(): void
